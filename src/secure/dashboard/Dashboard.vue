33968f4cacb04c5efb93f073cf07ba06c3176ff1
mohamedalaa253695 <mohamedalaa253695@gmail.com>
2022-01-30 01:27:54 +0200

    added orders chart to dashboard

--- src/secure/dashboard/Dashboard.vue
+++ src/secure/dashboard/Dashboard.vue
@@ -1,8 +1,47 @@
-<template>Dashboard</template>
+<template>
+  Dashboard
+  <div id="chart"></div>
+</template>
 
 <script>
-export default {};
+import { onMounted } from "vue";
+import axios from "axios";
+import * as c3 from "c3";
+
+export default {
+  name: "Dashboard",
+  setup() {
+    onMounted(async () => {
+      const chart = c3.generate({
+        bindto: "#chart",
+        data: {
+          x: "x",
+          columns: [["x"], ["Sales"]],
+          types: {
+            Sales: "bar",
+          },
+        },
+        axis: {
+          x: {
+            type: "timeseries",
+            tick: {
+              format: "%Y-%m-%d",
+            },
+          },
+        },
+      });
+
+      const response = await axios.get("chart");
+      const records = response.data.data;
+      chart.load({
+        columns: [
+          ["x", ...records.map((r) => r.date)],
+          ["Sales", ...records.map((r) => r.sum)],
+        ],
+      });
+    });
+  },
+};
 </script>
 
-<style>
-</style>
+<style></style>
